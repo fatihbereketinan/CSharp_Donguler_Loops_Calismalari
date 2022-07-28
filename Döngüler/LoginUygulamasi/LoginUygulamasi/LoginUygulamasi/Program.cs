bab41db2d3f30f2bbc77f1ba3039757366659de8
fatihbereketinan <fatihbereketinan@gmail.com>
2022-07-28 23:06:53 +0300

    CSharp_Donguler_Loops_Calismalari

---
+++ Döngüler/LoginUygulamasi/LoginUygulamasi/LoginUygulamasi/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LoginUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim = "Fatih";
+            string sifre = "qwerty";
+            int deneme = 0;
+
+            Console.WriteLine("*****Kullanıcı Giriş Sayfasına Hoş Geldiniz.*****");
+
+            while (true)
+            {
+                Console.Write("Lütfen kullanıcı adınızı giriniz: ");
+                string kullaniciadi = Console.ReadLine();
+                Console.Write("Lütfen sifrenizi giriniz: ");
+                string kullaniciSifresi = Console.ReadLine();
+
+                if (isim == kullaniciadi)
+                {
+                    if (sifre == kullaniciSifresi)
+                    {
+                        Console.WriteLine("Tebrikler başarılı bir şekilde giriş yaptınız..");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Kullanıcı adı veya şifresi hatalı. Lütfen tekrar deneyiniz.");
+                    deneme = deneme + 1;
+
+                    if (deneme == 3)
+                    {
+                        Console.WriteLine("3 kere hatalı giriş yaptınız. İşleminiz sonlandırılıyor...");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Döngüler/SayiYazdirma/SayiYazdirma/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SayiYazdirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1
+              12
+              123
+              .
+              .
+              123456789 şeklinde ekrana yazdırınız*/
+           
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
